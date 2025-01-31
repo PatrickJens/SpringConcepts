ecf24a85007b92f9e434ccc0079acb41a6c93a1d
Patrick Cannell <pcannell14@gmail.com>
2025-01-31 08:30:37 -0600

    testing git config

--- src/main/java/concepts/main/MainApplication.java
+++ src/main/java/concepts/main/MainApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+//small change for git config commit
 @SpringBootApplication
 public class MainApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(MainApplication.class, args);
+		int x = 1;
 	}
 
 	@Bean
