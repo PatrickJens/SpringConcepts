937ae46df1ba3625dde370f933a03aa8fabc3127
Patrick Cannell <pcannell14@gmail.com>
2025-02-05 19:29:17 -0600

    sql seed

---
+++ src/main/resources/sql/insert_students.sql
@@ -0,0 +1,9 @@
+##SELECT * FROM spring_student;
+
+INSERT INTO student_student
+(first_name, last_name, email) VALUES
+('Amy', 'Wright', 'awright@tamu.edu'),
+('Loretta', 'Rios', 'lrios@tamu.edu'),
+('Jackie', 'McMullen', 'jmcmullen@tamu.edu'),
+('Troy', 'Lopez', 'tlopez@tamu.edu'),
+('Celia', 'Zimmerman', 'czimmerman@tamu.edu')
